bikeshedder/tusker

commit 76f6432b4cddb71860a21275a52f1dc8e1c49be2

Add support for list of patterns for filename (#26)
Both `schema.filename` and `migrations.filename` now support a list of filenames. Plain strings are still supported.

Co-authored-by: Tim Beck <tim.beck@synauta.com>

tusker/config.py
@@ -17,7 +17,7 @@ class Config:
             data = {}
         # time to validate some configuration variables
         data.setdefault('database', {'dbname': 'tusker'})
-        data.setdefault('schema', {'filename': 'schema.sql'})
+        data.setdefault('schema', {'filename': ['schema.sql']})
         data.setdefault('migrations', {'directory': 'migrations'})
         data.setdefault('migra', {'safe': False, 'privileges': False})
         self.schema = SchemaConfig(data['schema'])
@@ -64,12 +64,21 @@ class ConfigReader:
             raise ConfigError.invalid(name, 'Not of type {}'.format(type))
         return value
 
+    def get_list(self, name, required=False, default=None):
+        value = self.get(name, (str, list), required, default)
+        if isinstance(value, str):
+            value = [value]
+        else:
+            if value and not all(isinstance(x, str) for x in value):
+                raise ConfigError.invalid(name, 'Not a list of strings {}'.format(value))
+        return value
+
 
 class SchemaConfig:
 
     def __init__(self, data):
         data = ConfigReader(data, 'schema')
-        self.filename = data.get('filename', str) or 'schema.sql'
+        self.filename = data.get_list('filename')
 
     def __str__(self):
         return 'SchemaConfig({!r})'.format(self.__dict__)
@@ -80,7 +89,8 @@ class MigrationsConfig:
     def __init__(self, data):
         data = ConfigReader(data, 'migrations')
         self.directory = data.get('directory', str, False)
-        self.filename = data.get('filename', str, False)
+        self.filename = data.get_list('filename')
+
         if not self.directory and not self.filename:
             self.directory = 'migrations'
         elif self.directory and self.filename:

tusker/__init__.py
@@ -109,9 +109,10 @@ class Tusker:
         with self.createdb('schema') as schema_engine:
             with schema_engine.begin() as schema_cursor:
                 self.log('Creating original schema...')
-                for filename in sorted(glob(self.config.schema.filename, recursive=True)):
-                    self.log('- {}'.format(filename))
-                    execute_sql_file(schema_cursor, filename)
+                for pattern in self.config.schema.filename:
+                    for filename in sorted(glob(pattern, recursive=True)):
+                        self.log('- {}'.format(filename))
+                        execute_sql_file(schema_cursor, filename)
             yield schema_engine
 
     @contextmanager
@@ -190,7 +191,8 @@ class Tusker:
     def _get_migrations(self):
         migrations = []
         if self.config.migrations.filename:
-            migrations = glob(self.config.migrations.filename, recursive=True)
+            for pattern in self.config.migrations.filename:
+                migrations.extend(glob(pattern, recursive=True))
         else:
             migrations = [
                 os.path.join(self.config.migrations.directory, filename)

commit 9f3be46acc5ff27308fd34253b2b0b7613e9d8a1

Add configuration default for schema filename
This fixes a regression from 76f6432b4cddb71860a21275a52f1dc8e1c49be2.

tusker/config.py
@@ -78,7 +78,7 @@ class SchemaConfig:
 
     def __init__(self, data):
         data = ConfigReader(data, 'schema')
-        self.filename = data.get_list('filename')
+        self.filename = data.get_list('filename', default=['schema.sql'])
 
     def __str__(self):
         return 'SchemaConfig({!r})'.format(self.__dict__)