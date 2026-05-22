fd2b123c997a6f6174e80839658d8285fc533da9
Michael P. Jung <michael.jung@terreon.de>
2026-05-22 14:29:05 +0000

    Enable additional linter rules

--- crates/tusker-query/src/lib.rs
+++ crates/tusker-query/src/lib.rs
@@ -1,6 +1,24 @@
 #![doc = include_str!("../README.md")]
-#![deny(nonstandard_style, rust_2018_idioms)]
+#![deny(
+    nonstandard_style,
+    rust_2018_idioms,
+    rustdoc::broken_intra_doc_links,
+    rustdoc::private_intra_doc_links
+)]
 #![forbid(non_ascii_idents, unsafe_code)]
+#![warn(
+    deprecated_in_future,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    unused_import_braces,
+    unused_labels,
+    unused_lifetimes,
+    unused_qualifications,
+    unused_results
+)]
+#![allow(clippy::uninlined_format_args)]
 
 use tokio_postgres::{types::ToSql, GenericClient, Row};
 
